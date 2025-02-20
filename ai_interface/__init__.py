0568768f52461e1f34f92423442468be6c9e525c
Artin Zarei <artin.zareie1383@gmail.com>
2025-02-20 21:44:35 +0330

    Added

---
+++ ai_interface/__init__.py
@@ -0,0 +1,2 @@
+from .list_models import list_models
+from .list_models import get_model_names
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cli
 import ui
-import ai_interface
+import ai_interface as ai
+from pprint import pprint # TODO: imported for debugging. Remove later.
 
 if __name__ == "__main__":
-    pass
+    pprint(ai.get_model_names(ai.list_models()))
